7b7439e4513505fa94505a58522309ad0acff590
NascimentoSilva <jns2@cin.ufpe.br>
2016-12-14 01:25:45 -0300

    adicionado funcionalidade 'detalhes'

--- escambo/urls.py
+++ escambo/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
+    url(r'^detalhes/(?P<pk>[0-9]+)/$', views.detalhes, name="detalhes"),
+    url(r'^novo/$', views.novo, name="novo"),
 ]
--- escambo/views.py
+++ escambo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,3 +6,11 @@ from .models import Post
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'escambo/home.html', {'posts' : posts})
+
+def detalhes(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'escambo/detalhes.html', {'post' : post})
+
+def novo(request):
+
+    return render(request, 'escambo/novo.html', {})
