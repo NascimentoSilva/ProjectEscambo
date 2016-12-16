7e948a5bbb45a1f410efb8155ff5f6dbbce926ad
NascimentoSilva <jns2@cin.ufpe.br>
2016-12-15 21:40:33 -0300

    configurando banco de dados

--- escambo/urls.py
+++ escambo/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import url, include
+from django.contrib import admin
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.home, name="home"),
-    url(r'^detalhes/(?P<pk>[0-9]+)/$', views.detalhes, name="detalhes"),
-    url(r'^novo/$', views.novo, name="novo"),
+    url(r'^$', views.home, name='home')
 ]
--- escambo/views.py
+++ escambo/views.py
@@ -1,16 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
-from .models import Post
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'escambo/home.html', {'posts' : posts})
-
-def detalhes(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'escambo/detalhes.html', {'post' : post})
-
-def novo(request):
-
-    return render(request, 'escambo/novo.html', {})
+    return render(request, 'escambo/home.html', {})
