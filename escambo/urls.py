0390d5d315d8819cab44b719c763cd45c48a4608
NascimentoSilva <jns2@cin.ufpe.br>
2016-12-16 00:49:11 -0300

    adicionado diretorio templates

--- escambo/urls.py
+++ escambo/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^detalhes/(?P<pk>[0-9]+)/$', views.detalhes, name="detalhes"),
+    url(r'^novo/$', views.novo, name="novo"),
+    url(r'^sobre/$', views.sobre, name="sobre"),
 ]
--- escambo/views.py
+++ escambo/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
-    return render(request, 'escambo/home.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'escambo/home.html', {'posts' : posts})
+
+def detalhes(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'escambo/detalhes.html', {'post' : post})
+
+def novo(request):
+
+    return render(request, 'escambo/novo.html', {})
+
+def sobre(request):
+    return render(request, 'escambo/sobre.html', {})
